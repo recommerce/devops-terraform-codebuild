951bf2259d038232e8f17b450da9600e95df692b
Vishal Meshram <vishalme@cybage.com>
2022-08-02 17:46:30 +0530

    output "name" block moved

--- outputs.tf
+++ outputs.tf
@@ -8,12 +8,12 @@ output "id" {
   value       = aws_codebuild_project.this.id
 }
 
-output "service_role_arn" {
-  description = "CodeBuild service role ARN."
-  value       = var.create_service_role ? module.codebuild_service_role.iam_role_arn : null
-}
-
 output "name" {
   description = "CodeBuild name."
   value       = aws_codebuild_project.this.name
+}
+
+output "service_role_arn" {
+  description = "CodeBuild service role ARN."
+  value       = var.create_service_role ? module.codebuild_service_role.iam_role_arn : null
 }
